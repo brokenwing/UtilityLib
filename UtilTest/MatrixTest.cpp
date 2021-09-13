d881a931a987c1694ed778b922a2a15cc329df48
linyang <linyang@asprova.com>
2021-09-13 16:22:29 +0900

    add matrix class and basic function

---
+++ UtilTest/MatrixTest.cpp
@@ -0,0 +1,252 @@
+#include "pch.h"
+#include "Matrix.h"
+
+using namespace Util;
+
+TEST( Matrix, Construction_empty_size )
+{
+	Matrix<int> mat1;
+	EXPECT_TRUE( mat1.empty() );
+	EXPECT_EQ( mat1.size(), std::make_pair( size_t( 0 ), size_t( 0 ) ) );
+
+	Matrix<int> mat2( 3, 4 );
+	EXPECT_FALSE( mat2.empty() );
+	EXPECT_EQ( mat2.size(), std::make_pair( size_t( 3 ), size_t( 4 ) ) );
+
+	Matrix<int> mat3( 3, 3, 1 );
+	EXPECT_FALSE( mat3.empty() );
+	EXPECT_EQ( mat3.size(), std::make_pair( size_t( 3 ), size_t( 3 ) ) );
+}
+
+TEST( Matrix, Construction_initializer_list )
+{
+	Matrix<int> mat( { {1,2},{3,4} } );
+	EXPECT_EQ( mat.size(), std::make_pair( size_t( 2 ), size_t( 2 ) ) );
+	EXPECT_EQ( mat[0][0], 1 );
+	EXPECT_EQ( mat[1][1], 4 );
+	EXPECT_EQ( mat[0][1], 2 );
+	EXPECT_EQ( mat[1][0], 3 );
+}
+
+TEST( Matrix, Construction_initializer_list_lack )
+{
+	Matrix<int> mat( { {1,2,3},{1,2},{1} } );
+	EXPECT_EQ( mat.size(), std::make_pair( size_t( 3 ), size_t( 3 ) ) );
+	EXPECT_EQ( mat[0][0], 1 );
+	EXPECT_EQ( mat[0][1], 2 );
+	EXPECT_EQ( mat[0][2], 3 );
+	EXPECT_EQ( mat[1][0], 1 );
+	EXPECT_EQ( mat[1][1], 2 );
+	EXPECT_EQ( mat[2][0], 1 );
+}
+
+TEST( Matrix, Construction_at )
+{
+	Matrix<int> mat( 3, 4, 1 );
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 4; j++ )
+			EXPECT_EQ( mat.at( i, j ), 1 );
+	mat.at( 0, 0 ) *= -1;
+	EXPECT_EQ( mat.at( 0, 0 ), -1 );
+}
+
+TEST( Matrix, Construction_bracket )
+{
+	Matrix<int> mat( 3, 4, 1 );
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 4; j++ )
+			EXPECT_EQ( mat[i][j], 1 );
+	mat[0][0] *= -1;
+	EXPECT_EQ( mat[0][0], -1 );
+}
+
+TEST( Matrix, swap )
+{
+	Matrix<int> mat1( 3, 3, 3 );
+	Matrix<int> mat2( 2, 2, 2 );
+	mat1.swap( mat2 );
+	ASSERT_EQ( mat1.size(), std::make_pair( size_t( 2 ), size_t( 2 ) ) );
+	ASSERT_EQ( mat2.size(), std::make_pair( size_t( 3 ), size_t( 3 ) ) );
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 3; j++ )
+			EXPECT_EQ( mat2[i][j], 3 );
+	for( int i = 0; i < 2; i++ )
+		for( int j = 0; j < 2; j++ )
+			EXPECT_EQ( mat1[i][j], 2 );
+}
+
+TEST( Matrix, fill )
+{
+	Matrix<int> mat( 3, 4, 1 );
+	mat.fill( -1 );
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 4; j++ )
+			EXPECT_EQ( mat[i][j], -1 );
+}
+
+TEST( Matrix, clear )
+{
+	Matrix<int> mat( 3, 4, 1 );
+	mat.clear();
+	EXPECT_TRUE( mat.empty() );
+}
+
+TEST( Matrix, resize )
+{
+	Matrix<int> mat;
+	mat.resize( 2, 3 );
+	EXPECT_EQ( mat.size(), std::make_pair( size_t( 2 ), size_t( 3 ) ) );
+	mat.resize( 3, 2 );
+	EXPECT_EQ( mat.size(), std::make_pair( size_t( 3 ), size_t( 2 ) ) );
+	mat.resize( 1, 1, 1 );
+	EXPECT_EQ( mat[0][0], 1 );
+}
+
+TEST( Matrix, equal )
+{
+	Matrix<int> mat1( 3, 4, 1 );
+	Matrix<int> mat2( 3, 4, 1 );
+	EXPECT_TRUE( mat1 == mat2 );
+	mat2[0][0] = 0;
+	EXPECT_FALSE( mat1 == mat2 );
+	EXPECT_FALSE( mat1 == Matrix<int>() );
+}
+
+TEST( Matrix, transpose )
+{
+	Matrix<int> mat(2,2);
+	mat[0][0] = 1;
+	mat[0][1] = 2;
+	mat[1][0] = 3;
+	mat[1][1] = 4;
+	mat.Transpose();
+	EXPECT_EQ( mat[0][0], 1 );
+	EXPECT_EQ( mat[1][1], 4 );
+	EXPECT_EQ( mat[0][1], 3 );
+	EXPECT_EQ( mat[1][0], 2 );
+}
+
+TEST( Matrix, MatrixPlus )
+{
+	Matrix<int> a(2,2,1);
+	Matrix<int> b( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = a + b;
+	EXPECT_EQ( c[0][0], 2 );
+	EXPECT_EQ( c[0][1], 3 );
+	EXPECT_EQ( c[1][0], 4 );
+	EXPECT_EQ( c[1][1], 5 );
+}
+
+TEST( Matrix, MatrixMinus )
+{
+	Matrix<int> a(2,2,1);
+	Matrix<int> b( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = b - a;
+	EXPECT_EQ( c[0][0], 0 );
+	EXPECT_EQ( c[0][1], 1 );
+	EXPECT_EQ( c[1][0], 2 );
+	EXPECT_EQ( c[1][1], 3 );
+}
+
+TEST( Matrix, MatrixMul )
+{
+	Matrix<int> a(2,2,3);
+	Matrix<int> b( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = a * b;
+	EXPECT_EQ( c[0][0], 3 );
+	EXPECT_EQ( c[0][1], 6 );
+	EXPECT_EQ( c[1][0], 9 );
+	EXPECT_EQ( c[1][1], 12 );
+}
+
+TEST( Matrix, MatrixDiv )
+{
+	Matrix<int> a(2,2,2);
+	Matrix<int> b( { {2,4},{6,8} } );
+	Matrix<int> c;
+	c = b / a;
+	EXPECT_EQ( c[0][0], 1 );
+	EXPECT_EQ( c[0][1], 2 );
+	EXPECT_EQ( c[1][0], 3 );
+	EXPECT_EQ( c[1][1], 4 );
+}
+
+TEST( Matrix, PlusNumber )
+{
+	Matrix<int> a( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = a + 1;
+	EXPECT_EQ( c[0][0], 2 );
+	EXPECT_EQ( c[0][1], 3 );
+	EXPECT_EQ( c[1][0], 4 );
+	EXPECT_EQ( c[1][1], 5 );
+}
+
+TEST( Matrix, MinusNumber )
+{
+	Matrix<int> a( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = a - 1;
+	EXPECT_EQ( c[0][0], 0 );
+	EXPECT_EQ( c[0][1], 1 );
+	EXPECT_EQ( c[1][0], 2 );
+	EXPECT_EQ( c[1][1], 3 );
+}
+
+TEST( Matrix, MulNumber )
+{
+	Matrix<int> a( { {1,2},{3,4} } );
+	Matrix<int> c;
+	c = a * 2;
+	EXPECT_EQ( c[0][0], 2 );
+	EXPECT_EQ( c[0][1], 4 );
+	EXPECT_EQ( c[1][0], 6 );
+	EXPECT_EQ( c[1][1], 8 );
+}
+
+TEST( Matrix, DivNumber )
+{
+	Matrix<int> a( { {2,4},{6,8} } );
+	Matrix<int> c;
+	c = a / 2;
+	EXPECT_EQ( c[0][0], 1 );
+	EXPECT_EQ( c[0][1], 2 );
+	EXPECT_EQ( c[1][0], 3 );
+	EXPECT_EQ( c[1][1], 4 );
+}
+
+TEST( Matrix, MatMul_nn )
+{
+	Matrix<int> a( { {1,2},{3,4} } );
+	Matrix<int> b( { {0,1},{1,0} } );
+	Matrix<int> c;
+	c = a.MatMul( b );
+	EXPECT_EQ( c[0][0], 2 );
+	EXPECT_EQ( c[0][1], 1 );
+	EXPECT_EQ( c[1][0], 4 );
+	EXPECT_EQ( c[1][1], 3 );
+}
+
+TEST( Matrix, MatMul_nm )
+{
+	Matrix<int> a( { {1,2,3},{4,5,6} } );
+	Matrix<int> b( { {1,0},{1,1},{0,1} } );
+	Matrix<int> c;
+	c = a.MatMul( b );
+	EXPECT_EQ( c[0][0], 3 );
+	EXPECT_EQ( c[0][1], 5 );
+	EXPECT_EQ( c[1][0], 9 );
+	EXPECT_EQ( c[1][1], 11 );
+}
+
+TEST( Matrix, Diagonal )
+{
+	Matrix<int> c = Matrix<int>::Diagonal( 2 );
+	EXPECT_EQ( c[0][0], 1 );
+	EXPECT_EQ( c[0][1], 0 );
+	EXPECT_EQ( c[1][0], 0 );
+	EXPECT_EQ( c[1][1], 1 );
+}
---
+++ UtilityLib/Matrix.h
@@ -0,0 +1,222 @@
+#pragma once
+#include "pch.h"
+#include <vector>
+
+namespace Util
+{
+template <typename T, typename Container = std::vector<std::vector<typename T>>>
+class Matrix
+{
+protected:
+	Container data;
+
+public:
+	Matrix()
+	{}
+	Matrix( size_t n, size_t m, const T val = T() )
+	{
+		resize( n, m, val );
+	}
+	Matrix( std::initializer_list<std::initializer_list<T>>&& mat )
+	{
+		size_t n = mat.size();
+		size_t m = 0;
+		for( auto& e : mat )
+			m = std::max( m, e.size() );
+
+		data.reserve( n );
+		for( auto& row : mat )
+		{
+			auto& cur = data.emplace_back();
+			cur.resize( m );
+			int idx = 0;
+			for( auto& e : row )
+				cur[idx++] = e;
+		}
+		assert( size() == std::make_pair( n, m ) );
+	}
+
+	//only number
+	static Matrix Diagonal( size_t n )
+	{
+		Matrix mat( n, n, 0 );
+		for( int i = 0; i < n; i++ )
+			mat.data[i][i] = 1;
+		return mat;
+	}
+
+	const typename Container::value_type& operator[]( int idx ) const	{		return data[idx];	}
+		  typename Container::value_type& operator[]( int idx )			{		return data[idx];	}
+	const T& at( int x, int y )const	{		return data[x][y];	}
+		  T& at( int x, int y )			{		return data[x][y];	}
+
+	void swap( Matrix& other )	{		data.swap( other.data );	}
+	void fill( const T& val )
+	{
+		for( auto& row : data )
+			for( auto& col : row )
+				col = val;
+	}
+	void clear()	{		data.clear();	}
+	void resize( size_t n, size_t m, const T val = T() )
+	{
+		data.clear();
+		data.resize( n );
+		for( auto& e : data )
+			e.resize( m, val );
+	}
+	bool empty()const	{		return data.empty();	}
+	std::pair<std::size_t, std::size_t> size()const
+	{
+		assert( selfcheck() );
+		if( data.empty() )
+			return std::make_pair( 0, 0 );
+		return std::make_pair( data.size(), data.front().size() );
+	}
+	void Transpose()
+	{
+		auto len = size();
+		for( int i = 0; i < len.first; i++ )
+			for( int j = i + 1; j < len.second; j++ )
+				std::swap( data[i][j], data[j][i] );
+	}
+	//Matrix multiplication
+	Matrix MatMul( const Matrix& other )const
+	{
+		auto la = size();
+		auto lb = other.size();
+		assert( la.second == lb.first );
+		Matrix c( la.first, lb.second );
+		for( int i = 0; i < la.first; i++ )
+			for( int j = 0; j < lb.second; j++ )
+				for( int k = 0; k < la.second; k++ )
+					c[i][j] += data[i][k] * other.data[k][j];
+		return c;
+	}
+
+	//operation with matrix
+
+	bool operator==( const Matrix& other )const
+	{
+		if( size() != other.size() )
+			return false;
+		auto l = size();
+		for( int i = 0; i < l.first; ++i )
+			for( int j = 0; j < l.second; ++j )
+				if( !( data[i][j] == other.data[i][j] ) )
+					return false;
+		return true;
+	}
+	Matrix operator+( const Matrix& other )const
+	{
+		Matrix c = *this;
+		Matrix::Apply( *this, other, c, [] ( const T& a, const T& b )->T
+		{
+			return a + b;
+		} );
+		return c;
+	}
+	Matrix operator-( const Matrix& other )const
+	{
+		Matrix c = *this;
+		Matrix::Apply( *this, other, c, [] ( const T& a, const T& b )->T
+		{
+			return a - b;
+		} );
+		return c;
+	}
+	Matrix operator*( const Matrix& other )const
+	{
+		Matrix c = *this;
+		Matrix::Apply( *this, other, c, [] ( const T& a, const T& b )->T
+		{
+			return a * b;
+		} );
+		return c;
+	}
+	Matrix operator/( const Matrix& other )const
+	{
+		Matrix c = *this;
+		Matrix::Apply( *this, other, c, [] ( const T& a, const T& b )->T
+		{
+			return a / b;
+		} );
+		return c;
+	}
+
+	//operation with number
+
+	template <typename T_Num>
+	Matrix operator+( const T_Num& val )const
+	{
+		Matrix c = *this;
+		c.Apply( [&val] ( T& x )->void
+		{
+			x += val;
+		} );
+		return c;
+	}
+	template <typename T_Num>
+	Matrix operator-( const T_Num& val )const
+	{
+		Matrix c = *this;
+		c.Apply( [&val] ( T& x )->void
+		{
+			x -= val;
+		} );
+		return c;
+	}
+	template <typename T_Num>
+	Matrix operator*( const T_Num& val )const
+	{
+		Matrix c = *this;
+		c.Apply( [&val] ( T& x )->void
+		{
+			x *= val;
+		} );
+		return c;
+	}
+	template <typename T_Num>
+	Matrix operator/( const T_Num& val )const
+	{
+		Matrix c = *this;
+		c.Apply( [&val] ( T& x )->void
+		{
+			x /= val;
+		} );
+		return c;
+	}
+
+protected:
+	bool selfcheck()const;
+
+private:
+	template<typename Func>
+	void Apply( Func func )//Apply function to each element
+	{
+		for( auto& row : data )
+			for( auto& e : row )
+				func( e );
+	}
+
+	template<typename Func>
+	static void Apply( const Matrix& a, const Matrix& b, Matrix& ret, Func func )//for each element: func(a,b)->ret
+	{
+		assert( a.size() == b.size() );
+		assert( a.size() == ret.size() );
+		auto l = ret.size();
+		for( int i = 0; i < l.first; ++i )
+			for( int j = 0; j < l.second; ++j )
+				ret.data[i][j] = func( a.data[i][j], b.data[i][j] );
+	}
+};
+
+template<typename T, typename Container>
+inline bool Matrix<T, Container>::selfcheck() const
+{
+	std::set<std::size_t> len;
+	for( auto& e : data )
+		len.insert( e.size() );
+	return len.size() <= 1;
+}
+}
