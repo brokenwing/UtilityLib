95090f490fc875102db67b87db3459f17d76e8c0
linyang <linyang@asprova.com>
2021-09-17 10:06:50 +0900

    add function in vecutil

--- UtilTest/VecUtil.cpp
+++ UtilTest/VecUtil.cpp
@@ -38,3 +38,123 @@ TEST( VecUtil, NEQ_diff_type )
 	EXPECT_TRUE( !NEQ( a, b.begin(), 3 ) );
 	EXPECT_TRUE( NEQ( a, c.begin(), 3 ) );
 }
+
+TEST( VecUtil, LT_str )
+{
+	char s1[] = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( LT( s1, s2, 3 ) );
+	EXPECT_TRUE( !LT( s1, s1, 3 ) );
+	EXPECT_TRUE( !LT( s1, s3, 3 ) );
+}
+
+TEST( VecUtil, LT_str_diff_type )
+{
+	std::string s1 = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( LT( s1.begin(), s2, 3 ) );
+	EXPECT_TRUE( !LT( s1.begin(), s1.begin(), 3 ) );
+	EXPECT_TRUE( !LT( s1.begin(), s3, 3 ) );
+}
+
+TEST( VecUtil, GT_str )
+{
+	char s1[] = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( !GT( s1, s2, 3 ) );
+	EXPECT_TRUE( !GT( s1, s1, 3 ) );
+	EXPECT_TRUE( GT( s1, s3, 3 ) );
+}
+
+TEST( VecUtil, GT_str_diff_type )
+{
+	std::string s1 = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( !GT( s1.begin(), s2, 3 ) );
+	EXPECT_TRUE( !GT( s1.begin(), s1.begin(), 3 ) );
+	EXPECT_TRUE( GT( s1.begin(), s3, 3 ) );
+}
+
+TEST( VecUtil, LE_str )
+{
+	char s1[] = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( LE( s1, s2, 3 ) );
+	EXPECT_TRUE( LE( s1, s1, 3 ) );
+	EXPECT_TRUE( !LE( s1, s3, 3 ) );
+}
+
+TEST( VecUtil, LE_str_diff_type )
+{
+	std::string s1 = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( LE( s1.begin(), s2, 3 ) );
+	EXPECT_TRUE( LE( s1.begin(), s1.begin(), 3 ) );
+	EXPECT_TRUE( !LE( s1.begin(), s3, 3 ) );
+}
+
+TEST( VecUtil, GE_str )
+{
+	char s1[] = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( !GE( s1, s2, 3 ) );
+	EXPECT_TRUE( GE( s1, s1, 3 ) );
+	EXPECT_TRUE( GE( s1, s3, 3 ) );
+}
+
+TEST( VecUtil, GE_str_diff_type )
+{
+	std::string s1 = "abc";
+	char s2[] = "abd";
+	char s3[] = "aad";
+	EXPECT_TRUE( !GE( s1.begin(), s2, 3 ) );
+	EXPECT_TRUE( GE( s1.begin(), s1.begin(), 3 ) );
+	EXPECT_TRUE( GE( s1.begin(), s3, 3 ) );
+}
+
+TEST( VecUtil, VecAdd )
+{
+	int arr[3] = { 1,2,3 };
+	int r[3] = { 2,3,4 };
+	VecAdd( arr, arr + 3, 1 );
+	EXPECT_TRUE( EQ( arr, r, 3 ) );
+}
+
+TEST( VecUtil, VecDec )
+{
+	int arr[3] = { 1,2,3 };
+	int r[3] = { 0,1,2 };
+	VecDec( arr, arr + 3, 1 );
+	EXPECT_TRUE( EQ( arr, r, 3 ) );
+}
+
+TEST( VecUtil, VecMul )
+{
+	int arr[3] = { 1,2,3 };
+	int r[3] = { 3,6,9 };
+	VecMul( arr, arr + 3, 3 );
+	EXPECT_TRUE( EQ( arr, r, 3 ) );
+}
+
+TEST( VecUtil, VecDiv )
+{
+	int arr[3] = { 2,4,6 };
+	int r[3] = { 1,2,3 };
+	VecDiv( arr, arr + 3, 2 );
+	EXPECT_TRUE( EQ( arr, r, 3 ) );
+}
+
+TEST( VecUtil, VecMod )
+{
+	int arr[3] = { 2,4,6 };
+	int r[3] = { 2,1,0 };
+	VecMod( arr, arr + 3, 3 );
+	EXPECT_TRUE( EQ( arr, r, 3 ) );
+}
--- UtilityLib/VecUtil.h
+++ UtilityLib/VecUtil.h
@@ -1,9 +1,59 @@
 #pragma once
 #include "pch.h"
 
-//vector computation
 namespace Util
 {
+//Vector Compare
+template <typename Iter_1, typename Iter_2>
+bool EQ( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+template <typename Iter_1, typename Iter_2>
+bool NEQ( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+template <typename Iter_1, typename Iter_2>
+bool LT( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+template <typename Iter_1, typename Iter_2>
+bool GT( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+template <typename Iter_1, typename Iter_2>
+bool LE( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+template <typename Iter_1, typename Iter_2>
+bool GE( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n );
+
+//Vector
+
+template <typename Iter, typename T>
+void VecAdd( Iter begin, Iter end, const T& val )
+{
+	for( auto i = begin; i != end; ++i )
+		*i += val;
+}
+template <typename Iter, typename T>
+void VecDec( Iter begin, Iter end, const T& val )
+{
+	for( auto i = begin; i != end; ++i )
+		*i -= val;
+}
+template <typename Iter, typename T>
+void VecMul( Iter begin, Iter end, const T& val )
+{
+	for( auto i = begin; i != end; ++i )
+		*i *= val;
+}
+template <typename Iter, typename T>
+void VecDiv( Iter begin, Iter end, const T& val )
+{
+	for( auto i = begin; i != end; ++i )
+		*i /= val;
+}
+template <typename Iter, typename T>
+void VecMod( Iter begin, Iter end, const T& val )
+{
+	for( auto i = begin; i != end; ++i )
+		*i %= val;
+}
+
+//
+//body
+//
+
 template <typename Iter_1, typename Iter_2>
 bool EQ( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
 {
@@ -18,5 +68,43 @@ template <typename Iter_1, typename Iter_2>
 bool NEQ( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
 {
 	return !EQ( vec_begin_1, vec_begin_2, n );
+}
+template <typename Iter_1, typename Iter_2>
+bool LT( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
+{
+	while( n-- )
+	{
+		if( *vec_begin_1 < *vec_begin_2 )
+			return true;
+		else if( *vec_begin_1 > *vec_begin_2 )
+			return false;
+		++vec_begin_1;
+		++vec_begin_2;
+	}
+	return false;//EQ
+}
+template <typename Iter_1, typename Iter_2>
+bool GT( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
+{
+	while( n-- )
+	{
+		if( *vec_begin_1 > *vec_begin_2 )
+			return true;
+		else if( *vec_begin_1 < *vec_begin_2 )
+			return false;
+		++vec_begin_1;
+		++vec_begin_2;
+	}
+	return false;//EQ
+}
+template <typename Iter_1, typename Iter_2>
+bool LE( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
+{
+	return !GT( vec_begin_1, vec_begin_2, n );
+}
+template <typename Iter_1, typename Iter_2>
+bool GE( Iter_1 vec_begin_1, Iter_2 vec_begin_2, size_t n )
+{
+	return !LT( vec_begin_1, vec_begin_2, n );
 }
 }
