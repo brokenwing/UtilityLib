29e05ece327180aea49cde5d9cf3f4beb8fc0d9c
MSI-TRIDENTX\13616 <13616020402@163.com>
2023-05-02 16:58:19 +0900

    add new disjointset

--- UtilTest/DisjointSet.cpp
+++ UtilTest/DisjointSet.cpp
@@ -40,3 +40,31 @@ TEST( DisjointSet, size )
 	ds.Merge( 1, 7 );
 	EXPECT_EQ( ds.size(), 7 );
 }
+
+TEST( FastDisjointSet, basic )
+{
+	FastDisjointSet ds;
+	ds.resize( 5 );
+	ds.Merge( 1, 2 );
+	ds.Merge( 3, 4 );
+	EXPECT_EQ( ds.GetRoot( 1 ), ds.GetRoot( 2 ) );
+	EXPECT_EQ( ds.GetRoot( 3 ), ds.GetRoot( 4 ) );
+	ds.Merge( 1,4 );
+	EXPECT_EQ( ds.GetRoot( 1 ), ds.GetRoot( 3 ) );
+	EXPECT_EQ( ds.GetRoot( 1 ), ds.GetRoot( 4 ) );
+	EXPECT_EQ( ds.GetRoot( 2 ), ds.GetRoot( 3 ) );
+	EXPECT_EQ( ds.GetRoot( 2 ), ds.GetRoot( 4 ) );
+}
+TEST( FastDisjointSet, count )
+{
+	FastDisjointSet ds;
+	ds.resize( 7 );
+	ds.Merge( 1, 2 );
+	ds.Merge( 3, 4 );
+	ds.Merge( 5, 6 );
+	EXPECT_EQ( ds.Count(), 4 );
+	ds.Merge( 1,4 );
+	EXPECT_EQ( ds.Count(), 3 );
+	ds.Merge( 3,5 );
+	EXPECT_EQ( ds.Count(), 2 );
+}
--- UtilityLib/DisjointSet.h
+++ UtilityLib/DisjointSet.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "CommonDef.h"
+#include <vector>
 
 namespace Util
 {
-//并查集
+//并查集 (General)
 template <typename T = int, typename Hasher = std::hash<T>, typename isEQ = std::equal_to<T>>
 class DisjointSet
 {
@@ -57,12 +58,10 @@ public:
 	size_t Count()
 	{
 		ComputeRootOfEachElement();
-		LFA::unordered_set<T, Hasher, isEQ> tmp;
+		int tot = 0;
 		for( auto& e : m_id2parent )
-		{
-			tmp.insert( e.second );
-		}
-		return tmp.size();
+			tot += e.first == e.second;
+		return tot;
 	}
 
 	void Merge( T A, T B )
@@ -86,6 +85,87 @@ public:
 	void reserve( size_t size )
 	{
 		m_id2parent.reserve( size );
+	}
+};
+
+//range is limited, only for int
+class FastDisjointSet
+{
+private:
+	std::vector<int> m_id2parent;// [id]=parent
+
+	int find( const int id )
+	{
+		int& node = m_id2parent[id];
+		if( id != node )//is not root
+		{
+			node = find( node );//set to root
+		}
+		return node;
+	}
+public:
+	FastDisjointSet()
+	{}
+	FastDisjointSet( size_t size )
+	{
+		resize( size );
+	}
+	~FastDisjointSet()
+	{}
+
+	typename decltype( m_id2parent )::const_iterator begin()const noexcept
+	{
+		return m_id2parent.begin();
+	}
+	typename decltype( m_id2parent )::const_iterator end()const noexcept
+	{
+		return m_id2parent.end();
+	}
+	std::size_t size()const
+	{
+		return m_id2parent.size();
+	}
+
+	bool isSameGroup( int A, int B )
+	{
+		A = find( A );
+		B = find( B );
+		return A == B;
+	}
+	int GetRoot( const int id )
+	{
+		return find( id );
+	}
+	//number of group
+	size_t Count()
+	{
+		ComputeRootOfEachElement();
+		int tot = 0;
+		const int n = static_cast<int>( m_id2parent.size() );
+		for( int i = 0; i < n; ++i )
+			tot += i == m_id2parent[i];
+		return tot;
+	}
+
+	void Merge( int A, int B )
+	{
+		A = find( A );
+		B = find( B );
+		if( A != B )
+			m_id2parent[A] = B;
+	}
+	void ComputeRootOfEachElement()
+	{
+		for( int&p : m_id2parent )
+		{
+			p = find( p );
+		}
+	}
+	void resize( size_t size )
+	{
+		m_id2parent.resize( size );
+		for( int i = 0; i < size; i++ )
+			m_id2parent[i] = i;
 	}
 };
 }
